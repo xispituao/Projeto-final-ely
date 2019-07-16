8cf5f64eb7c82fd809ced9558116fa09f73d5a24
john-emerson14 <john.carv.sousa@gmail.com>
2019-07-16 08:59:42 -0300

    modificando o logout no urls.py

--- connectedin/connectedin/urls.py
+++ connectedin/connectedin/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('/<int:contato_id>', views.desfazer_amizade, name='desfazer_amizade'),
     path('registrar/', RegistrarUsuarioView.as_view(), name='registrar'),
     path('login/', v.LoginView.as_view(template_name='login.html'), name='login'),
-    path('logout/', v.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', v.LogoutView.as_view(template_name='login.html'), name='logout'),
 
 ]
 
